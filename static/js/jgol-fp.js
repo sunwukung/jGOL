1ea5948d2ef9817de39682e6c8886f5ccae49bcf
Marcus Kielly <marcus@marcusk.co.uk>
2012-11-09 22:28:38 +0000

    implementing toroidal transformations

--- static/js/jgol-fp.js
+++ static/js/jgol-fp.js
@@ -3,80 +3,107 @@ var jgol = (function (jgol) {
 	var posInt, 
 		dimension, 
 		makeCells,
-		toroidal, 
-		bounded,
-		lookUp;
+		scan,
+		offset,
+		torusOffset, 
+		boundOffset;
 
-	// PRIVATE FUNCTIONS
+	// PRIVATE 
 	// =========================================================
 
+
+	// TRANSFORMERS
+	// ---------------------------------------------------------
+	torusOffset = function (sum, lo, hi) {
+		return sum > hi 
+			? (sum - hi) - 1
+			: sum < lo 
+				? (hi + sum) + 1
+				: sum;
+	};
+
+	boundOffset = function (sum, lo, hi) {
+		return sum > hi 
+			? hi 
+			: sum < lo
+				? lo
+				: sum;
+	};
+
+
+	jgol.offset = function (origin, offset, boundary, bound) {
+		return bound 
+			? boundOffset(origin + offset, 0, boundary) 
+			: torusOffset(origin + offset, 0, boundary);
+	};
+
+
+	jgol.delta = function (row, col, cells, bound) {
+		return _.map([[-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 0],[0, 1],[1, -1],[1, 0],[1, 1]
+			], function (offset) {
+				return [
+					jgol.offset(row, offset[0], cells.length, bound), 
+					jgol.offset(col, offset[1], cells[0].length, bound)
+				];
+			});
+	};
+
+
+	scan = function (row, col, cells, toroidal) {
+
+		/*
+		return _.map(delta(cells, toroidal), function (xy) {
+			return cells
+				.slice(row + xy[0],(row + xy[0]) + 1)[0]
+				.slice(col + xy[1],(col + xy[1]) + 1)[0];
+			});
+*/
+	};	
+
+
+	// GENERATORS
+	// ---------------------------------------------------------
 
 	posInt = function (i) {
 		return parseInt(i) > 0;
 	}; 
 
-
 	dimension = function (w, h) {
 		return (posInt(w) && posInt(h)) ? w * h : false;
 	};
+
+
+	makeCells = function (rows, cols) {
+		return _.map(_.range(rows), function(){
+			return _.map(_.range(cols), function () {
+				return 0;
+			});
+		});
+	};
+
+
 	
-
-	lookUp = function (index, cols) {
-		return [
-			(index - cols) - 1,
-			index - 1,
-			(index + cols) - 1
-		];
-	};	
-
-	collect = function (cells, lu) {
-		return cells
-			.slice(lu[0],lu[0] + 3)
-			.concat(cells.slice(lu[1],lu[1]+1))
-			.concat(cells.slice(lu[2],lu[2]+3));
-	};
-
-
-	makeCells = function (d) {
-		return _.map(_.range(d), function(){
-			return false;});
-	};
-
-
-	toroidal = function (index, cols, cells) {
-		return collect(cells, lookUp(index, cols));	
-	};
-
-
-	bounded = function(index, cols, cells) {
-		return collect(cells, lookUp(index, cols));
-	};
 
 
 	// PUBLIC FUNCTIONS
 	// =========================================================
 
 
-	jgol.findLive = function (index, cols, cells, bound){
-		var collected =  bound ? 
-			bounded(index, cols, cells) :
-			toroidal(index, cols, cells);
+	jgol.findLive = function (row, col, cells, bound){
+		var scanned =  bound ? 
+			scan(row, col, cells, false) :
+			scan(row, col, cells, true);
 
-		return _.reduce(collected,
-			function (collector, c) {
-				return collector + c;
-			});
+		return _.reduce(scanned,
+			function (sum, cell) {
+				return sum + cell;
+			}) - 1;
 	};
 
 
 	jgol.generate = function (cols, rows) {
 		var d = dimension(cols, rows);
-		return d > 0 ?
-		{
-			cols : cols,
-			cells : makeCells(d)
-		} : 
-		false;
+		return d > 0 ? makeCells(cols, rows) : false;
 	};
 
 
--- tests/jgol-fptests.js
+++ tests/jgol-fptests.js
@@ -1,15 +1,12 @@
 module('jgol-calc', {
 	setup : function () {
-		this.fixA = {
-			cols : 5,
-			cells : [
-				0,0,0,0,0,
-				0,1,0,0,0,
-				0,0,1,0,0,
-				0,0,0,1,0,
-				0,0,0,0,0,
+		this.fixA = [
+				[0,0,0,0,0],
+				[0,1,0,0,0],
+				[0,0,1,0,0],
+				[0,0,0,1,0],
+				[0,0,0,0,0],
 			]
-		};
 	}
 });
 
@@ -26,15 +23,69 @@ test('generate', function () {
 		badFieldB = jgol.generate('foo', 1),
 		badFieldC = jgol.generate(0, 1);
 
-	ok(q.isO(field), 'jgol.field -> object');
-	equal(field.cols, 3, 'jgol.field = (w * h)');
+	ok(q.isA(field), 'jgol.field -> array');
+	equal(field.length, 3, 'jgol.field rows === w');
+	equal(field[0].length, 3, 'jgol.field cols === h');
 
 	equal(badFieldA, false, 'gol.generate(float, int) -> false ');
 	equal(badFieldB, false, 'gol.generate(string, int) -> false ');
 	equal(badFieldC, false, 'gol.generate(0, int) -> false ');
 });
 
-test('findLive:toroidal', function () {
-	var n = jgol.findLive(12, 5, this.fixA.cells);
-	equal(n, 2, 'jgol.findLive -> sum of live adjacent cells');
+test('offset:torus', function () {
+	var torusA = jgol.offset(0, -1, 4),
+		torusB = jgol.offset(4, 1, 4);
+
+	equal(torusA, 4, '0 - 1 -> 4');
+	equal(torusB, 0, '4 + 1 -> 0');
 });
+
+
+test('offset:bound', function () {
+	var boundA = jgol.offset(0, -1, 4, true),
+		boundB = jgol.offset(4, 1, 4, true);
+
+	equal(boundA, 0, '0 - 1 -> 0');
+	equal(boundB, 4, '4 + 1 -> 4');
+});
+
+test('delta', function () {
+	var delta = jgol.delta(2, 2, this.fixA),
+		exp = [
+			[1,1],[1,2],[1,3],
+			[2,1],[2,2],[2,3],
+			[3,1],[3,2],[3,3]
+		];
+	deepEqual(delta, exp, 'delta');
+});
+
+test('delta:torus', function () {
+	var delta = jgol.delta(0, 0, this.fixA),
+		exp = [
+			[4,4],[4,0],[4,1],
+			[0,4],[0,0],[0,1],
+			[1,4],[1,0],[1,1]
+		];
+		console.log(delta);
+	deepEqual(delta, exp, 'delta');
+});
+
+test('delta:bound', function () {
+	var delta = jgol.delta(0, 0, this.fixA, true),
+		exp = [
+			[0,0],[0,0],[0,1],
+			[0,0],[0,0],[0,1],
+			[1,0],[1,0],[1,1]
+		];
+	deepEqual(delta, exp, 'delta used bound');
+});
+
+
+
+/*
+test('findLive:toroidal', function () {
+	var a = jgol.findLive(2, 2, this.fixA),
+		b = jgol.findLive(0, 0, this.fixA);
+	equal(n, 1, 'jgol.findLive -> sum of live adjacent cells');
+});
+*/
